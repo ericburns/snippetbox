aac8e48b1714c37368a8b1b3817385bba1ad82e8
Eric Burns <ejburns@pm.me>
2021-08-29 17:53:34 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module ericburns.io/snippetbox
+
+go 1.16
+
+require (
+	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golangcollege/sessions v1.2.0 // indirect
+	github.com/justinas/alice v1.2.0 // indirect
+	github.com/justinas/nosurf v1.1.1 // indirect
+	golang.org/x/crypto v0.0.0-20200317142112-1b76d66859c6 // indirect
+)
---
+++ pkg/models/models.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"errors"
+	"time"
+)
+
+var ErrNoRecord = errors.New("models: no matching record found")
+var ErrDuplicateEmail = errors.New("models: duplicate email")
+var ErrInvalidCredentials = errors.New("models: invalid credentials")
+
+type Snippet struct {
+	ID      int
+	Title   string
+	Content string
+	Created time.Time
+	Expires time.Time
+}
+
+type User struct {
+	ID             int
+	Name           string
+	Email          string
+	HashedPassword []byte
+	Created        time.Time
+	Active         bool
+}
